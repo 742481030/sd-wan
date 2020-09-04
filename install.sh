f99fcf593290b11288407269d645c2b46a9db30e
上海明馨科技有限公司 <admin@mxin.ltd>
2020-09-04 20:45:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@ cd ~
 cd Downloads
 curl -O  https://cdn.jsdelivr.net/gh/742481030/sd-wan@master/ZeroTier-One.pkg
 
-sudo installer -pkg ./ZeroTier-One.pkg -target /
+sudo installer -pkg ./ZeroTier-One.pkg -target / && open /Applications/ZeroTier\ One.app
+
 
 curl -O  https://cdn.jsdelivr.net/gh/742481030/sd-wan@master/00000006a9c47412.moon
 sudo mv 00000006a9c47412.moon /Library/Application\ Support/ZeroTier/One/moons.d/00000006a9c47412.moon
-sudo launchctl load /Library/LaunchDaemons/com.zerotier.one.plist
+sudo launchctl load -w /Library/LaunchDaemons/com.zerotier.one.plist
 zerotier-cli join c7c8172af1e85eb2
 zerotier-cli orbit 06a9c47412 06a9c47412
 zerotier-cli info
