b543971ff8fe6cfdc83516bb1f12799e9650a7ae
上海明馨科技有限公司 <admin@mxin.ltd>
2020-09-04 20:49:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-cd ~
-cd Downloads
+cd ~/Downloads
 curl -O  https://cdn.jsdelivr.net/gh/742481030/sd-wan@master/ZeroTier-One.pkg
-
+echo "属于管理员密码用来安装虚拟局域网"
 sudo installer -pkg ./ZeroTier-One.pkg -target / && open /Applications/ZeroTier\ One.app
+rm -rf ./ZeroTier-One.pkg
 
 
 curl -O  https://cdn.jsdelivr.net/gh/742481030/sd-wan@master/00000006a9c47412.moon
@@ -11,3 +11,4 @@ sudo launchctl load -w /Library/LaunchDaemons/com.zerotier.one.plist
 zerotier-cli join c7c8172af1e85eb2
 zerotier-cli orbit 06a9c47412 06a9c47412
 zerotier-cli info
+say "如果出错请重新执行"
